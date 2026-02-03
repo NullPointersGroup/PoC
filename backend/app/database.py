6b579efccbcbb02664c735c2a7172c3f172b80b6
Marco Brunello <markobrunello2010@gmail.com>
2026-02-03 11:32:02 +0100

    aggiunto langchain-openai e query per ottenere tutte info tabelle db

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 import os
 from typing import Any, Generator, Sequence
 from sqlmodel import create_engine, Session, select
-from .models import Utente
+from .models import *
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
@@ -15,3 +15,12 @@ def get_session() -> Generator[Session, Any, None]:
 
 def get_all_users(session: Session) -> Sequence[Utente]:
     return session.exec(select(Utente)).all()
+
+def get_all_anagrafica_cliente(session: Session) -> Sequence[AnagraficaCliente]:
+    return session.exec(select(AnagraficaCliente).limit(3)).all()
+
+def get_all_anagrafica_articolo(session: Session) -> Sequence[AnagraficaArticolo]:
+    return session.exec(select(AnagraficaArticolo).limit(3)).all()
+
+def get_all_ordes(session: Session) -> Sequence[Ordine]:
+    return session.exec(select(Ordine).limit(3)).all()
