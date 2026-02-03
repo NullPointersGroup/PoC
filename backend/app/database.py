0dde3e4eb782a7a686570e9227586710fffbb097
every-go <matteoevery498@gmail.com>
2026-02-03 10:17:58 +0100

    Collegato col .env

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,13 +1,14 @@
+import os
 from typing import Any, Generator, Sequence
 from sqlmodel import create_engine, Session, select
 from .models import Utente
 
-# Prendi l'URL dal docker-compose environment
-DATABASE_URL = "postgresql://user:password@db:5432/mydb"
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL non impostata")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
-# Funzione per ottenere una sessione
 def get_session() -> Generator[Session, Any, None]:
     with Session(engine) as session:
         yield session
