9c089c153283fb922d43040c2784c5bc4d21b4c9
every-go <matteoevery498@gmail.com>
2026-02-03 12:30:03 +0100

    Ok

--- backend/app/AI.py
+++ backend/app/AI.py
@@ -1,14 +1,12 @@
 from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
 from langchain.tools import tool
-
-
+from typing import Any
 
 model = ChatOpenAI(
     model="gpt-5",
     temperature=0.1,
-    max_tokens=1000,
+    model_kwargs={"max_tokens": 1000},
     timeout=30
-    # ... (other params)
 )
-agent = create_agent(model)
+agent: Any = create_agent(model)
